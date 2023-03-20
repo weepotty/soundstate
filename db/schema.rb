ffbc0ba9aa733c06f49f217e5e90c6d71f2786b7
Shona Chan <mojavealoe@gmail.com>
2023-03-20 18:00:45 +0800

    creates playlist model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_093359) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_095857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_093359) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "playlists", force: :cascade do |t|
+    t.boolean "is_favourite", default: false
+    t.bigint "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_playlists_on_event_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,4 +54,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_093359) do
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "playlists", "events"
 end
