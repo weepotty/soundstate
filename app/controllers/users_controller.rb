7bdc7e6b0d853c2d126826c7078dfa9990a66135
Shona Chan <mojavealoe@gmail.com>
2023-03-21 20:03:38 +0800

    filters user's saved tracks depending on an event

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,23 +8,25 @@ class UsersController < ApplicationController
     combined_tracks.concat(@tracks)
     offset = 50
 
+    features_array = []
+
+    @event = Event.find(41)
+
+
     while @tracks.count == 50
       @tracks = spotify_user.saved_tracks(offset:, limit: 50)
-      combined_tracks.concat(@tracks)
+      features_array.concat(RSpotify::AudioFeatures.find(@tracks.map(&:id)))
+      # combined_tracks.concat(@tracks)
       offset += 50
     end
 
-    combined_tracks.count
-    features_array = RSpotify::AudioFeatures.find(@tracks.map(&:id))
-
-    @event = Event.find(50)
     copy_array = features_array.select do |feature|
-      feature.acousticness.between?(@event.acousticness - 0.2, @event.acousticness + 0.2)
-        # feature.danceability.between?(@event.danceability - 0.5, @event.danceability + 0.5) &&
-        # feature.energy.between?(@event.energy - 0.5, @event.energy + 0.5) &&
-        # feature.instrumentalness.between?(@event.instrumentalness - 0.5, @event.instrumentalness + 0.5) &&
-        # feature.tempo.between?(@event.tempo - 50, @event.tempo + 50) &&
-        # feature.valence.between?(@event.valence - 0.5, @event.valence + 0.5)
+      feature.acousticness.between?(@event.acousticness - 0.5, @event.acousticness + 0.5) &&
+        feature.danceability.between?(@event.danceability - 0.5, @event.danceability + 0.5) &&
+        feature.energy.between?(@event.energy - 0.5, @event.energy + 0.5) &&
+        feature.instrumentalness.between?(@event.instrumentalness - 0.5, @event.instrumentalness + 0.5) &&
+        feature.tempo.between?(@event.tempo - 40, @event.tempo + 40) &&
+        feature.valence.between?(@event.valence - 0.5, @event.valence + 0.5)
     end
 
 raise
