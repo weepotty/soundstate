2fac785a6b1799643ed072bdbc3b6770931e8898
Shona Chan <mojavealoe@gmail.com>
2023-03-21 11:48:45 +0800

    adds dependent destroy to user model for events and playlists

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  has_many :events
-  has_many :playlists
+  has_many :events, dependent: :destroy
+  has_many :playlists, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
