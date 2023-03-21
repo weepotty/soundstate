745db6c3df15e78f251c9489854ac2310d700ef0
Shona Chan <mojavealoe@gmail.com>
2023-03-21 11:01:39 +0800

    add spotify id to playlists table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_025832) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_030047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_025832) do
     t.datetime "updated_at", null: false
     t.string "title", null: false
     t.bigint "user_id", null: false
+    t.string "spotify_id", null: false
     t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
