a4337eda616f2f494e322cb52bfdd06a6a9f6074
Shona Chan <mojavealoe@gmail.com>
2023-03-21 20:27:28 +0800

    adds comments to code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,24 @@
 class UsersController < ApplicationController
   def spotify
+    # create an RSpotify User
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     User.create_user(spotify_user)
 
-    combined_tracks = []
+    # get the audio features of the first 50 tracks from user's library
     @tracks = spotify_user.saved_tracks(offset: 0, limit: 50)
-    combined_tracks.concat(@tracks)
+    features_array = RSpotify::AudioFeatures.find(@tracks.map(&:id))
+
+    # if user has more than 50 songs in library, keep fetching audio features
     offset = 50
-
-    features_array = []
-
-    @event = Event.find(41)
-
-
     while @tracks.count == 50
       @tracks = spotify_user.saved_tracks(offset:, limit: 50)
       features_array.concat(RSpotify::AudioFeatures.find(@tracks.map(&:id)))
-      # combined_tracks.concat(@tracks)
       offset += 50
     end
 
-    copy_array = features_array.select do |feature|
+    # filter the audio features according to event's filters
+    @event = Event.find(41)
+    filtered_array = features_array.select do |feature|
       feature.acousticness.between?(@event.acousticness - 0.5, @event.acousticness + 0.5) &&
         feature.danceability.between?(@event.danceability - 0.5, @event.danceability + 0.5) &&
         feature.energy.between?(@event.energy - 0.5, @event.energy + 0.5) &&
@@ -28,7 +26,11 @@ class UsersController < ApplicationController
         feature.tempo.between?(@event.tempo - 40, @event.tempo + 40) &&
         feature.valence.between?(@event.valence - 0.5, @event.valence + 0.5)
     end
+    raise
 
-raise
+    # get the spotify song URIs from the filtered_array and create a playlist with these +/- store in user's spotify
+    # play the playlist
+
   end
+
 end
