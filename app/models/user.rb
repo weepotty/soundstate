53e225a5d745f6fb6c667a0232a7ed72fb5fddfa
Shona Chan <mojavealoe@gmail.com>
2023-03-21 11:16:38 +0800

    removes has_many playlists through events  association for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :events
-  has_many :playlists, through: :events
+  has_many :playlists
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
