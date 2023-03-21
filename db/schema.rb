64bb0f6c6a4b3be36f157c0b4be28be9057b7e9d
Shona Chan <mojavealoe@gmail.com>
2023-03-21 10:58:10 +0800

    removes event id from playlists table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_025216) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_025535) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,11 +34,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_025216) do
 
   create_table "playlists", force: :cascade do |t|
     t.boolean "is_favourite", default: false
-    t.bigint "event_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title", null: false
-    t.index ["event_id"], name: "index_playlists_on_event_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,5 +53,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_025216) do
   end
 
   add_foreign_key "events", "users"
-  add_foreign_key "playlists", "events"
 end
