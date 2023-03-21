5757e9633ffeeff3b836f0cff604567d866d96b6
Shona Chan <mojavealoe@gmail.com>
2023-03-21 11:00:24 +0800

    adds user id to playlists table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_025535) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_025832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_025535) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,4 +55,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_025535) do
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "playlists", "users"
 end
