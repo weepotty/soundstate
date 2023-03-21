38cfc05997c8190f23d13d22fb5cfe22ead2c914
Shona Chan <mojavealoe@gmail.com>
2023-03-21 11:02:34 +0800

    removes has_many playlists from event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many :playlists
 
   validates :title, presence: true, length: { maximum: 25 }
   validates_presence_of %i[acousticness danceability energy instrumentalness liveness speechiness tempo valence popularity]
