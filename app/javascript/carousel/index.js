232a02fe5020edf0e09e6e00706ef187da2a5769
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-21 15:20:27 +0800

    Import flickity js and css and add css for carousel

---
+++ app/javascript/carousel/index.js
@@ -0,0 +1,16 @@
+// Import the Flickity library
+import Flickity from "flickity";
+
+// Wait for content load
+window.addEventListener("load", function() {
+
+// Initialise Flickity
+var flkty = new Flickity(".carousel", {
+        //Flickity options
+});
+
+// Due to the implementation of Flickity, a resize is required.
+// This can be accomplished by manually resizing the browser viewport
+// or do it here with JS see: https://github.com/metafizzy/flickity/issues/205
+flkty.resize();
+});
