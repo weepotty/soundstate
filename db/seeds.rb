1b49563f50938379f93c165ba35c9f086fa809d6
Shona Chan <mojavealoe@gmail.com>
2023-03-21 12:04:38 +0800

    seed file - starts with destroying all playlists, events and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "destroying playlists..."
+Playlist.destroy_all
+
+puts "destroying events..."
+Event.destroy_all
+
+puts "destroying users..."
+User.destroy_all
+
+puts 'planting new seeds'
+
+User.create(email: 'frank@frankdev.com', password: '123456', nickname: 'frank')
+User.create(email: 'sally@sallydev.com', password: '123456', nickname: 'sally')
+User.create(email: 'bob@bobdev.com', password: '123456', nickname: 'bob')
+User.create(email: 'carol@caroldev.com', password: '123456', nickname: 'carol')
+User.create(email: 'alice@alicedev.com', password: '123456', nickname: 'alice')
+
+Event.create(title: 'Getting ready', acousticness: 0.5, danceability: 0.2, energy: 0.8, instrumentalness: 0.5,  )
+
+puts 'seeded like a lovely garden'
