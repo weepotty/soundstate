46461149e96fd83ec86362a174b38c211a127f41
Shona Chan <mojavealoe@gmail.com>
2023-03-22 15:37:22 +0800

    finally fixed the seeds

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,7 +1,8 @@
 class Playlist < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
-  has_many :songs
+  has_many :playlists_songs
+  has_many :songs, through: :playlists_songs
 
   validates :title, presence: true, length: { maximum: 25 }
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -117,9 +117,7 @@ users.each do |user|
   )
 end
 
-Playlist.create!
 
-playlist1 = Playlist.create!(title: 'watering flowers', user: user1, is_shared: true)
 
 filepath = '/Users/shona/code/weepotty/soundstate/db/songs.csv'
 
@@ -127,62 +125,35 @@ CSV.foreach(filepath, headers: :first_row) do |row|
   Song.create!(spotify_id: row['spotify_id'], name: row['name'], uri: row['uri'], artist: row['artist'], acousticness: row['acousticness'], danceability: row['danceability'], energy: row['energy'], instrumentalness: row['instrumentalness'], tempo: row['tempo'], valence: row['valence'])
 end
 
-playlist1 = Playlist.create!(title:'watering plants', user: user1, is_shared: true)
+playlist1 = Playlist.create!(title: 'watering flowers', user: user1, is_shared: true)
 playlist2 = Playlist.create!(title: 'George & Indiana', user: user2)
 playlist3 = Playlist.create!(title: 'Stretching', user: user3, is_shared: true)
 playlist4 = Playlist.create!(title: 'SHUIJIAO', user: user4, is_shared: true)
 playlist5 = Playlist.create!(title: 'secret yoga', user: user5, is_shared: true)
 
-# Song.all.sample(50).each do |song|
-#   PlaylistsSong.create!(playlist: playlist1, song:)
-#   SongsUser.create!(song:, user: user1)
-# end
-# Song.all.sample(50).each do |song|
-#   PlaylistsSong.create!(playlist: playlist1, song:)
-#   SongsUser.create!(song:, user: user1)
-# end
-# Song.all.sample(50).each do |song|
-#   PlaylistsSong.create!(playlist: playlist1, song:)
-#   SongsUser.create!(song:, user: user1)
-# end
-# Song.all.sample(50).each do |song|
-#   PlaylistsSong.create!(playlist: playlist1, song:)
-#   SongsUser.create!(song:, user: user1)
-# end
+Song.all.sample(50).each do |song|
+  PlaylistsSong.create!(playlist: playlist1, song:)
+  SongsUser.create!(song:, user: user1)
+end
 
+Song.all.sample(50).each do |song|
+  PlaylistsSong.create!(playlist: playlist2, song:)
+  SongsUser.create!(song:, user: user2)
+end
 
+Song.all.sample(50).each do |song|
+  PlaylistsSong.create!(playlist: playlist3, song:)
+  SongsUser.create!(song:, user: user3)
+end
 
-# Playlist.create!(title: 'SHUI JIAO', spotify_id: '1BUSEFH8QA7cpuq8x5NUkF', user: user1, is_shared: true)
-# Playlist.create!(title: '12/03/22', spotify_id: '39h6GUFlCwuIjIL9SNhgGn', user: user1)
-# Playlist.create!(title: 'Warm up', spotify_id: '3Zi3WP45w3w3sMCSye5tqD', user: user1, is_shared: true)
-# Playlist.create!(title: '2020', spotify_id: '7djWk93hoTVGMIoP3WQHmM', user: user1)
-# Playlist.create!(title: '2019.04.18', spotify_id: '2iqNnWem8Uy2fs5zqj8YNZ', user: user1, is_shared: true)
-# Playlist.create!(title: 'Jazz bossanova and stuff', spotify_id: '2HIgZySlv8HR4gE3kY1x5B', user: user1, is_shared: true)
+Song.all.sample(50).each do |song|
+  PlaylistsSong.create!(playlist: playlist4, song:)
+  SongsUser.create!(song:, user: user4)
+end
 
-# Playlist.create!(title: 'millenials', spotify_id: '6JoAVBBqi5JmdnmWELRnJa', user: user2)
-# Playlist.create!(title: 'chill but no chill', spotify_id: '6dRv9kj05Ylq5XlAtpqrjs', user: user2)
-# Playlist.create!(title: 'i love edm', spotify_id: '23I0EHbnpWbhchGbd5WBIp', user: user2, is_shared: true)
-# Playlist.create!(title: 'boss', spotify_id: '0nLC6GaJCjejdoDqscLT1M', user: user2)
-# Playlist.create!(title: 'secret yoga', spotify_id: '2sDakL9bYMnDT4ieERyIBD', user: user2)
-
-# Playlist.create!(title: 'Temporary', spotify_id: '6OgDAJp86TNVuFqt3vi4cL', user: user3, is_shared: true)
-# Playlist.create!(title: 'Baguette', spotify_id: '45JEE6q0iDO0Y6QDottVTj', user: user3)
-# Playlist.create!(title: 'Boss WORKOUUUTT', spotify_id: '2WHJK2Q007wOOnfQCJZ7nM', user: user3)
-# Playlist.create!(title: 'song', spotify_id: '5zhaSMGjMFq8PiIPgb8Ham', user: user3, is_shared: true)
-# Playlist.create!(title: 'stretching', spotify_id: '0mAeoOnm1vVOWRg1wy6AFT', user: user3, is_shared: true)
-
-# Playlist.create!(title: 'for my friends', spotify_id: '1UCY5fAnCk6w590OuAWvFS', user: user4, is_shared: true)
-# Playlist.create!(title: 'make myself calm', spotify_id: '7sht7porXURcSQPeYMbR7E', user: user4)
-# Playlist.create!(title: 'hiking', spotify_id: '1QasLhQdvGsQW7Si6HLW2s', user: user4, is_shared: true)
-# Playlist.create!(title: 'wonderful calm', spotify_id: '5r5GApHpvB3O6IUl3O9D8t', user: user4, is_shared: true)
-# Playlist.create!(title: 'musical therapy', spotify_id: '1uRn1Xd3AeitXDO8xkdd0n', user: user4, is_shared: true)
-# Playlist.create!(title: 'yoga yoga', spotify_id: '6id7SVOIFSbXHNrWrUL6Vn', user: user4, is_shared: true)
-
-# Playlist.create!(title: 'code monkey', spotify_id: '0mWhUI9HdprDlrY2JGXN1o', user: user5, is_shared: true)
-# Playlist.create!(title: 'silence and calm', spotify_id: '4pwTaUwO5HNwQA3OsPFEDP', user: user5, is_shared: true)
-# Playlist.create!(title: 'cycling', spotify_id: '1UoGc2m2g3IbepT1YJpNvG', user: user5, is_shared: true)
-# Playlist.create!(title: 'chilling', spotify_id: '6iXQpDitArksnLfprwx3OV', user: user5, is_shared: true)
-# Playlist.create!(title: 'alpha waves', spotify_id: '7jfg6SX0LJYseu71JmZ4RK', user: user5, is_shared: true)
-# Playlist.create!(title: 'BOOM', spotify_id: '4G48L9Cas3KLUvCGx7Q2aS', user: user5, is_shared: true)
+Song.all.sample(50).each do |song|
+  PlaylistsSong.create!(playlist: playlist5, song:)
+  SongsUser.create!(song:, user: user5)
+end
 
 # puts 'seeded like a lovely garden'
