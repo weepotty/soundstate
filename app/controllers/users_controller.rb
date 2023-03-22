97570136659887e73b7b345c6e014217e45de90e
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-22 16:28:52 +0800

    Adds user sign in and user register path for user spotify login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,15 @@ class UsersController < ApplicationController
   def spotify
     # create an RSpotify User
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    User.create_user(spotify_user)
+    @user = User.create_user(spotify_user)
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Spotify'
+      sign_in_and_redirect @user, event: :authentication
+    else
+      # Removing extra as it can overflow some session stores
+      session['devise.spotify_data'] = request.env['omniauth.auth'].except('extra')
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
 
     # get all the from user's library
     @tracks = spotify_user.saved_tracks(offset: 0, limit: 50)
