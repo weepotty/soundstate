260d07ff5d2d2f5fd8125c1e81f6e5960592ee72
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-03-23 11:04:41 +0800

    Generate playlist controller

---
+++ app/controllers/playlists_controller.rb
@@ -0,0 +1,19 @@
+class PlaylistsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'playlists/index'
+  get 'playlists/show'
+  get 'playlists/create'
+  get 'playlists/edit'
+  get 'playlists/update'
+  get 'playlists/delete'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
