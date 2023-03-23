037f95ab9c3edfe219d3b7286eae4e196ef05eac
Shona Chan <mojavealoe@gmail.com>
2023-03-23 10:21:18 +0800

    adds presence and uniqueness validations on playlistssongs table

--- app/models/playlists_song.rb
+++ app/models/playlists_song.rb
@@ -1,4 +1,8 @@
 class PlaylistsSong < ApplicationRecord
   belongs_to :playlist
   belongs_to :song
+
+  validates :playlist, presence: true
+  validates :song, presence: true
+  validates :playlist, uniqueness: { scope: :song }
 end
