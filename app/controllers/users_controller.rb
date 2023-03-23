67d217734d1763f65bea9240f5a6d6ccc41e59dd
Shona Chan <mojavealoe@gmail.com>
2023-03-23 16:04:59 +0800

    user#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,12 @@ class UsersController < ApplicationController
       current_user: current_user
     )
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
 end
