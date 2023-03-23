85963cd01362d48e99ef49760cce6263d214fa6a
Shona Chan <mojavealoe@gmail.com>
2023-03-23 16:22:58 +0800

    adds html for search bar testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,11 +21,12 @@ class UsersController < ApplicationController
     )
   end
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
-  def index
-    @users = User.all
-  end
 end
