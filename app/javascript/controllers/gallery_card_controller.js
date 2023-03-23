03aa98a4eb664b6c17a6e722ff89316581f80906
Faris Chew <farischew@gmail.com>
2023-03-23 16:46:34 +0800

    Makes cards flip back if user flips another card

--- app/javascript/controllers/gallery_card_controller.js
+++ app/javascript/controllers/gallery_card_controller.js
@@ -4,7 +4,10 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["card"]
 
-  click () {
-    this.cardTarget.classList.toggle('is-flipped');
+  click (e) {
+    const all_cards = document.querySelectorAll('[data-gallery-card-target="card"]')
+    all_cards.forEach((c)=>c.classList.remove("is-flipped"))
+
+    this.cardTarget.classList.add("is-flipped")
   }
 }
