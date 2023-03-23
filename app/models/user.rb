8e67462cad43e36c61f19e988eae1c2345e3e55f
Shona Chan <mojavealoe@gmail.com>
2023-03-23 16:24:40 +0800

    adds pg_search_scope to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,11 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true, uniqueness: true
 
+  pg_search_scope :search_by_nickname,
+    against: [ :nickname ],
+    using: {
+      tsearch: { prefix: true }
+    }
   def self.create_user(spotify_user)
     account = User.where(email: spotify_user.email).first
     account ||= User.new(email: spotify_user.email, password: Devise.friendly_token[0, 20], nickname: spotify_user.display_name)
