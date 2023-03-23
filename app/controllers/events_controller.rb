bcade2ff7b14d58336a846b5969f632af13c4d16
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-23 21:42:41 +0800

    Adjusted the tempo in the events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,7 +9,7 @@ class EventsController < ApplicationController
     @event = Event.new(event_params)
     @event.user = current_user
     if @event.save
-      redirect_to root_path
+      redirect_to new_event_playlist_path(@event)
     else
       render :new, status: :unprocessable_entity
     end
@@ -18,6 +18,9 @@ class EventsController < ApplicationController
   # GET /events/:id/edit
   def edit
     @event = Event.find(params[:id])
+    # 220 tempo for the maximum range, to fit slider of 0 to 1
+    @event.min_tempo = (@event.min_tempo / 220)
+    @event.max_tempo = (@event.max_tempo / 220)
   end
 
   # PATCH /events/:id
@@ -34,6 +37,8 @@ class EventsController < ApplicationController
 
   def event_params
     params[:event][:time] = params[:event][:time].to_i
+    params[:event][:min_tempo] = params[:event][:min_tempo].to_f * 220
+    params[:event][:max_tempo] = params[:event][:max_tempo].to_f * 220
     params.require(:event).permit(:title,
                                   :min_acousticness, :max_acousticness,
                                   :min_danceability, :max_danceability,
