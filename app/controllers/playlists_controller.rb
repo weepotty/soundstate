43cbb2f2cb71c88e2fcc9abce3005f50132b1a76
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-03-24 13:13:17 +0800

    Created show page for playlists

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -9,21 +9,15 @@ class PlaylistsController < ApplicationController
   # GET /playlists/:id
   def show
     # show requires an "id" variable to insert into embeded iframe
-    @playlist = Playlist.includes(:spotify_id).find(params[:id])
+    @playlist = Playlist.find(params[:id])
   end
 
   # GET /events/:event_id/playlists/new
   def new
     @playlist = Playlist.new
-    # get the event that we will filter the songs by
+
     @event = Event.find(params[:event_id])
-
-    # filter the user's songs with event
     filter_songs(@event)
-
-    @song_uris
-
-    @tracks = RSpotify::Base.find(@song_ids.first(20), 'track')
   end
 
   # POST /events/:event_id/playlists
@@ -31,15 +25,18 @@ class PlaylistsController < ApplicationController
     # access user's account
     spotify_user = current_user.spotify_user
 
+    @event = Event.find(params[:event_id])
+    filter_songs(@event)
+
     # create instance of playlist to spotify
-    @spotify_playlist = spotify_user.create_playlist!(playlist_params.title)
+    @spotify_playlist = spotify_user.create_playlist!(playlist_params[:title])
 
     # add tracks to spotify playlist by
-    @spotify_playlist.add_tracks!(@song_uris.sample(100))
-
-    # create instacne of playlist in out database
-    # no image yet!
-    @ss_playlist = Playlist.create!(title: playlist_params.title, user: current_user, spotify_id: @spotify_playlist.id)
+    @spotify_playlist.add_tracks!(@song_uris.sample(20))
+    @ss_playlist = Playlist.new(title: playlist_params[:title], user: current_user, spotify_id: @spotify_playlist.id)
+     if @ss_playlist.save!
+      redirect_to playlist_path(@ss_playlist)
+     end
   end
 
   private
@@ -56,11 +53,6 @@ class PlaylistsController < ApplicationController
     @song_uris = []
     @songs.each do |song|
       @song_uris << song.uri
-    end
-
-    @song_ids = []
-    @songs.each do |song|
-      @song_ids << song.spotify_id
     end
   end
 
