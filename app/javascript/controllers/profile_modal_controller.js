fc978f99875f35a7689c1fb37330713c3e1770b2
Faris Chew <farischew@gmail.com>
2023-03-25 18:44:23 +0800

    Adds stimulus controller and targets

--- app/javascript/controllers/profile_modal_controller.js
+++ app/javascript/controllers/profile_modal_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="profile-modal"
 export default class extends Controller {
+  static targets = ["modal", "overlay", "close", "open"]
+
   connect() {
+    console.log("Connected");
   }
 }
