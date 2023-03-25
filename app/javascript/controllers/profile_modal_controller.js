a7c6e2aa8848dc508756c4194f5299ff0b6ac9c6
Faris Chew <farischew@gmail.com>
2023-03-25 22:08:56 +0800

    Cleaned up controller

--- app/javascript/controllers/profile_modal_controller.js
+++ app/javascript/controllers/profile_modal_controller.js
@@ -4,19 +4,11 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["modal", "overlay"]
 
-  connect() {
-    console.log("Connected");
-  }
-
   open() {
-    console.log("Open modal");
     this.modalTarget.classList.remove("hidden")
-    this.overlayTarget.classList.remove("hidden")
   }
 
   close() {
-    console.log("Close modal");
     this.modalTarget.classList.add("hidden")
-    this.overlayTarget.classList.add("hidden")
   }
 }
