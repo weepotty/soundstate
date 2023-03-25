f6a29294383c410918bbc81926e4f6b653f71aa9
Faris Chew <farischew@gmail.com>
2023-03-25 21:01:21 +0800

    Adds js to open and close modal

--- app/javascript/controllers/profile_modal_controller.js
+++ app/javascript/controllers/profile_modal_controller.js
@@ -2,9 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="profile-modal"
 export default class extends Controller {
-  static targets = ["modal", "overlay", "close", "open"]
+  static targets = ["modal", "overlay"]
 
   connect() {
     console.log("Connected");
+  }
+
+  open() {
+    console.log("Open modal");
+    this.modalTarget.classList.remove("hidden")
+    this.overlayTarget.classList.remove("hidden")
+  }
+
+  close() {
+    console.log("Close modal");
+    this.modalTarget.classList.add("hidden")
+    this.overlayTarget.classList.add("hidden")
   }
 }
