1463a646e58d8e61c656cb17ab43deccc65db066
shona <weepotty@gmail.com>
2023-03-28 23:06:05 +0800

    flips gallery card back to image side when pop up is closed

--- app/javascript/controllers/profile_modal_controller.js
+++ app/javascript/controllers/profile_modal_controller.js
@@ -1,19 +1,24 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="profile-modal"
 export default class extends Controller {
-  static targets = ["modal", "overlay", "share"]
+  static targets = ["modal", "overlay", "share", "card"];
 
+  connect() {
+    console.log("hello from the profile-modal controller");
+    console.log(this.cardTarget);
+  }
   open() {
     setTimeout((event) => {
-      this.modalTarget.classList.remove("hidden")
+      this.modalTarget.classList.remove("hidden");
     }, 500);
     console.log(this.event);
   }
 
   close() {
     setTimeout(() => {
-      this.modalTarget.classList.add("hidden")
+      this.modalTarget.classList.add("hidden");
     }, 500);
+    this.cardTargets.forEach((c) => c.classList.remove("is-flipped"));
   }
 }
