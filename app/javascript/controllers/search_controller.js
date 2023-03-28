8a9547de80eb3653b4a6fc15934c1b512070e55f
Faris Chew <farischew@gmail.com>
2023-03-28 15:04:59 +0800

    Adds controller and targets

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,6 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search"
 export default class extends Controller {
+  static targets = ["form", "input", "list"]
+
   connect() {
+    console.log(this.formTarget)
+    console.log(this.inputTarget)
+    console.log(this.listTarget)
   }
 }
