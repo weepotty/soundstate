1f5e137f2f3928937b0d45f7e191ff48572cdeac
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-28 11:53:26 +0800

    Overrides redirection to root after user sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  # resource is the User instance created from users#spotify
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
 end
