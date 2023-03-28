3b8ade7e326a12abc79855b2a9c901837a75b815
Shona Chan <mojavealoe@gmail.com>
2023-03-28 11:43:05 +0800

    hides create playlist button until image has been previewed

--- app/javascript/controllers/image_creation_controller.js
+++ app/javascript/controllers/image_creation_controller.js
@@ -2,17 +2,25 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="image-creation"
 export default class extends Controller {
-  static targets = ["playlist", "image"];
+  static targets = [
+    "playlist",
+    "image",
+    "generateImageButton",
+    "createPlaylistButton",
+  ];
 
   connect() {
     console.log("connected");
     console.log(this.playlistTarget);
     console.log(this.imageTarget);
+    console.log(this.generateImageButtonTarget);
+    console.log(this.createPlaylistButtonTarget);
   }
 
   showImage() {
     this.playlistTarget.classList.add("d-none");
     this.imageTarget.classList.remove("d-none");
-    this.generateImageButton.classList.add("d-none");
+    this.generateImageButtonTarget.classList.add("d-none");
+    this.createPlaylistButtonTarget.classList.remove("d-none");
   }
 }
