cc9a8af967ddcb36fdd2f19a01db45e47341efac
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-28 17:27:07 +0800

    Moves the avatar input to click on the avatar

--- app/javascript/controllers/profile_picture_upload_controller.js
+++ app/javascript/controllers/profile_picture_upload_controller.js
@@ -2,9 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="profile-picture-upload"
 export default class extends Controller {
-  static targets = ["profilePicture"];
+  static targets = ["profilePicture", "fileInput"];
   
   connect() {
+  }
+
+  uploadPicture(event) {
+    console.log(this.fileInputTarget)
+    this.fileInputTarget.click()
   }
 
   changePicture(event) {
