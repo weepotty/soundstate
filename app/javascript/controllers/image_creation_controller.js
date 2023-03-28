93871414b11866b4932818b6a78f28a5682b5c7e
Shona Chan <mojavealoe@gmail.com>
2023-03-28 11:30:32 +0800

    adds sections for image display and adds stimulus target

--- app/javascript/controllers/image_creation_controller.js
+++ app/javascript/controllers/image_creation_controller.js
@@ -2,7 +2,17 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="image-creation"
 export default class extends Controller {
+  static targets = ["playlist", "image"];
+
   connect() {
     console.log("connected");
+    console.log(this.playlistTarget);
+    console.log(this.imageTarget);
+  }
+
+  showImage() {
+    this.playlistTarget.classList.add("d-none");
+    this.imageTarget.classList.remove("d-none");
+    this.generateImageButton.classList.add("d-none");
   }
 }
