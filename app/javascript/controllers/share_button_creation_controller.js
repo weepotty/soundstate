dda8d0242a4ec7028076f933a4ed38c519f1db40
Faris Chew <farischew@gmail.com>
2023-03-29 12:18:14 +0800

    Adds action and target for share button

--- app/javascript/controllers/share_button_creation_controller.js
+++ app/javascript/controllers/share_button_creation_controller.js
@@ -2,7 +2,27 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="share-button-creation"
 export default class extends Controller {
-  connect() {
-    console.log("Share button connected");
-  }
+  static targets = ["share"]
+
+  share() {
+    const url = `/playlists/toggle_shared`
+    const unlock = "<p class='m-0' style='$light-gray'>Playlist Shared</p>"
+
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        playlist_id: this.idValue
+      })
+  })
+    .then(res => res.json())
+    .then((data) => {
+      this.shareTarget.outerHTML = unlock
+      console.log(this.shareTarget);
+  })
+}
 }
