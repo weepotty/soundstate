f805ea88bb4ca24d44778d29e34bf419aa31d734
Faris Chew <farischew@gmail.com>
2023-03-29 12:04:12 +0800

    Cleaned up share-button controller

--- app/javascript/controllers/share_button_creation_controller.js
+++ app/javascript/controllers/share_button_creation_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="share-button-creation"
 export default class extends Controller {
   connect() {
+    console.log("Share button connected");
   }
 }
