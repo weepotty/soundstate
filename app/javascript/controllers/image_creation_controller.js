7c9f76152df8cb1336981af0302edccb56468dfa
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-30 16:09:21 +0800

    Adds loading message to the image generation loading

--- app/javascript/controllers/image_creation_controller.js
+++ app/javascript/controllers/image_creation_controller.js
@@ -43,7 +43,7 @@ export default class extends Controller {
   generateImage() {
     return new Promise((resolve, reject) => {
       this.imageTarget.innerHTML =
-        "<div class='playlist-loader'></div><div class='playlist-loader-text'>Generating AI Art...</div>";
+        "<div class='playlist-loader'><div>LOADING</div></div><div class='playlist-loader-text'>Generating AI Art...</div>";
 
       fetch(`/events/${this.eventidValue}/image`, {
         headers: {
