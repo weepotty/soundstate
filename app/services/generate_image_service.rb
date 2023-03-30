1b36b360fa0dddeb10231831608d25a1c317d84e
shona <weepotty@gmail.com>
2023-03-30 12:40:12 +0800

    adds more art styles

--- app/services/generate_image_service.rb
+++ app/services/generate_image_service.rb
@@ -6,12 +6,11 @@ class GenerateImageService
     # Various prompt helper words for better image generation results.
     art_styles = ['pop art', 'risograph', 'illustration', 'cubism', 'memphis', 'digital art',
                   '3D render', 'block printing', 'watercolor', 'synthwave', 'ceramics', 'vaporwave', 'linocut art',
-                  'geometric drawing', 'line art', 'vintage', '3D illustration', 'lego bricks', 'claymation', 'isometric art',
-                  'salvador dali', 'van gogh', 'bauhaus', 'low poly']
-
+                  'geometric drawing', 'line art', 'vintage', '3D illustration', 'lego bricks', 'claymation',
+                  'salvador dali', 'van gogh', 'low poly']
     # Generate image and returns image url.
     client = OpenAI::Client.new
-    image_response = client.images.generate(parameters: { prompt: "#{prompt}, #{mood_descriptors(event).sample(2).join(', ')}, in the art style of #{art_styles.sample}. use a colour palette inspired by #{time_colour_descriptor.sample}", size: "256x256" })
+    image_response = client.images.generate(parameters: { prompt: "#{prompt}, #{mood_descriptors(event).sample(2).join(', ')}, in the art style of #{art_styles.sample}. use a colour palette inspired by #{time_colour_descriptor(event).sample}", size: "256x256" })
 
     # Return the url of the image generated.
     img_res = image_response.dig('data', 0, 'url')
@@ -34,12 +33,14 @@ class GenerateImageService
     end
   end
 
-  def time_colour_descriptor(event)
+  def self.time_colour_descriptor(event)
     case event.time
     when 'morning'
       %w[spring dawn]
     when 'evening'
       %w[dusk twilight]
+    when 'afternoon'
+      %w[summer autumn]
     end
   end
 
