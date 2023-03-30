1bdf0ce6ecdb50b364bc19148f9452357bc79c24
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-03-30 17:43:44 +0800

    removed tool tip

--- app/javascript/controllers/tooltip_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-import { Tooltip } from "bootstrap/dist/js/bootstrap.bundle.js";
-
-// Connects to data-controller="tooltip"
-export default class extends Controller {
-  connect() {
-    new Tooltip(this.element);
-  }
-}
