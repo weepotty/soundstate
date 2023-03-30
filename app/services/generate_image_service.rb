3743363469632490f7a899e1588122a1f2dc5aba
shona <weepotty@gmail.com>
2023-03-30 10:02:29 +0800

    edits art styles and includes colour descriptors based on time of day

--- app/services/generate_image_service.rb
+++ app/services/generate_image_service.rb
@@ -5,11 +5,13 @@ class GenerateImageService
 
     # Various prompt helper words for better image generation results.
     art_styles = ['pop art', 'risograph', 'illustration', 'cubism', 'memphis', 'digital art',
-                  '3D render', 'block printing', 'watercolor', 'synthwave', 'ceramics', 'vaporwave', 'linocut art']
+                  '3D render', 'block printing', 'watercolor', 'synthwave', 'ceramics', 'vaporwave', 'linocut art',
+                  'geometric drawing', 'line art', 'vintage', '3D illustration', 'lego bricks', 'claymation', 'isometric art',
+                  'salvador dali', 'van gogh', 'bauhaus', 'low poly']
 
     # Generate image and returns image url.
     client = OpenAI::Client.new
-    image_response = client.images.generate(parameters: { prompt: "#{prompt}, #{mood_descriptors(event).sample(2).join(', ')}, in the art style of #{art_styles.sample}", size: "256x256" })
+    image_response = client.images.generate(parameters: { prompt: "#{prompt}, #{mood_descriptors(event).sample(2).join(', ')}, in the art style of #{art_styles.sample}. use a colour palette inspired by #{time_colour_descriptor.sample}", size: "256x256" })
 
     # Return the url of the image generated.
     img_res = image_response.dig('data', 0, 'url')
@@ -18,17 +20,26 @@ class GenerateImageService
   # Helper method to get descriptions to help enhance the image generated.
   def self.mood_descriptors(event)
     # happy energetic
-    if event.max_valence > 0.5 && event.max_energy > 0.5
+    if event.max_valence > 0.6 && (event.max_energy > 0.6 || event.max_acousticness < 0.5)
       %w[bright vibrant dynamic spirited vivid lively energetic colorful joyful romantic expressive rich]
     # happy chill
-    elsif event.max_valence > 0.5 && event.max_energy < 0.5
+    elsif event.max_valence > 0.6 && (event.max_energy < 0.6 || event.max_acousticness > 0.5)
       %w[light peaceful calm serene soothing relaxed cosy tranquil pastel ethereal tender soft]
     # sad chill
     elsif event.max_valence < 0.4
-      %w[winter somber melancholic sad gothic post-apocalyptic]
+      %w[winter somber melancholic sad gothic post-apocalyptic poignant]
     # discordant moods
     else
-      %w[sublime symmetrical vibrant vivid poignant]
+      %w[sublime symmetrical cool]
+    end
+  end
+
+  def time_colour_descriptor(event)
+    case event.time
+    when 'morning'
+      %w[spring dawn]
+    when 'evening'
+      %w[dusk twilight]
     end
   end
 
