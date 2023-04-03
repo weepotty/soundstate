4109dbb9a97273580b595dceb4e86a17f4755a8f
shona <weepotty@gmail.com>
2023-04-03 22:04:48 +0800

    restores 10 iframe limit on homepage

--- app/javascript/controllers/homepage_controller.js
+++ app/javascript/controllers/homepage_controller.js
@@ -1,18 +1,17 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="homepage"
 export default class extends Controller {
-  static targets = ['hide']
+  static targets = ["hide"];
   connect() {
-
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('active');
+          entry.target.classList.add("active");
         } else {
-          entry.target.classList.remove('active');
+          entry.target.classList.remove("active");
         }
-      })
+      });
     });
 
     this.hideTargets.forEach((el) => observer.observe(el));
