1d28e988f7d1846c6f411c5c54689cafb264371e
shona <weepotty@gmail.com>
2023-04-04 15:52:24 +0800

    adds protol 2 to config redis server

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -5,6 +5,7 @@ url = ENV["REDISCLOUD_URL"]
 if url
   Sidekiq.configure_server do |config|
     config.redis = { url: url }
+    config.redis = { protocol: 2 }
   end
 
   Sidekiq.configure_client do |config|
