5e44634e11ed6b7e209326404a8bb152d1a4aef4
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-16 16:17:24 +0800

    Configure system test to take screenshot on failed tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,5 +9,12 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  # Devise test helpers
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
   # Add more helper methods to be used by all tests here...
 end
+
+# Folder path for screenshots
+Capybara.save_path = Rails.root.join("tmp/capybara")
