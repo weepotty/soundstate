71f50267e7b45a97e22d1feefb3b5acb826aca14
Richard Bates <rich.bates@protonmail.com>
2023-04-19 22:55:30 +0800

    Moves call to instance method and initializes song and event instance variables

--- app/services/generate_image_service.rb
+++ app/services/generate_image_service.rb
@@ -13,10 +13,19 @@ class GenerateImageService
   }.freeze
 
   def self.call(song:, event:)
+    new(song:, event:).call
+  end
+
+  def initialize(song:, event:)
+    @song = song
+    @event = event
+  end
+
+  def call
     # Various prompt helper words for better image generation results.
     image_response = client.images.generate(
       parameters: {
-        prompt: "#{generated_prompt(song)},#{mood_descriptors(event).sample(2).join(', ')}, in the art style of #{ART_STYLES.sample}. use a colour palette inspired by #{time_colour_descriptor(event).sample}",
+        prompt: prompt_params,
         size: '256x256'
       }
     )
@@ -25,8 +34,35 @@ class GenerateImageService
     image_response.dig('data', 0, 'url')
   end
 
+  private
+
+  attr_accessor :song, :event
+
+  def prompt_params
+    <<~HEREDOC
+      #{generated_prompt},
+      #{mood_descriptors.sample(2).join(', ')},
+      in the art style of #{ART_STYLES.sample}.
+      use a colour palette inspired by #{TIME_COLOURS[event.time].sample}"
+    HEREDOC
+  end
+
+  # Private method to generate prompt from the Song instance object.
+  def generated_prompt
+    query = "Return the mood of the song #{song.name} by #{song.artist} in three words."
+    response = client.chat(
+      parameters: {
+        model: 'gpt-3.5-turbo', # Required.
+        messages: [{ role: 'user', content: query }], # Required.
+        temperature: 0.7
+      }
+    )
+
+    response.dig('choices', 0, 'message', 'content')
+  end
+
   # Helper method to get descriptions to help enhance the image generated.
-  def self.mood_descriptors(event)
+  def mood_descriptors
     # very sad songs
     if event.max_valence < 0.4
       %w[winter somber melancholic sad gothic post-apocalyptic poignant]
@@ -45,25 +81,7 @@ class GenerateImageService
     end
   end
 
-  def self.time_colour_descriptor(event)
-    TIME_COLOURS[event.time]
-  end
-
-  # Private method to generate prompt from the Song instance object.
-  def self.generated_prompt(song)
-    query = "Return the mood of the song #{song.name} by #{song.artist} in three words."
-
-    response = client.chat(
-      parameters: {
-        model: 'gpt-3.5-turbo', # Required.
-        messages: [{ role: 'user', content: query }], # Required.
-        temperature: 0.7
-      }
-    )
-    response.dig('choices', 0, 'message', 'content')
-  end
-
-  def self.client
-    OpenAI::Client.new
+  def client
+    @client ||= OpenAI::Client.new
   end
 end
