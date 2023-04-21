fecf6460041b40b8d07f9178493abbd402961704
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-22 03:12:37 +0800

    Extract the method to create_song

--- app/services/import_songs_service.rb
+++ app/services/import_songs_service.rb
@@ -10,24 +10,22 @@ class ImportSongsService
 
   def call
     import_songs
-
-    # get the spotify song URIs from the filtered_array and create a playlist with these +/- store in user's spotify
-    # play the playlist
   end
 
   private
 
   attr_reader :spotify_user, :current_user
 
-  def fetch_all_songs
+  def fetch_all_tracks
     all_tracks = []
     offset = 0
-    # Limit import songs to 400, to limit api request.
+
     loop do
       tracks = spotify_user.saved_tracks(offset:, limit: 50)
       all_tracks.concat(tracks)
       offset += 50
-
+      
+      # Limit import songs to 400, to limit api request.
       break unless (tracks.count == 50 && offset < 400)
     end
 
@@ -35,28 +33,29 @@ class ImportSongsService
   end
 
   def import_songs
-    fetch_all_songs.each do |track|
+    fetch_all_tracks.each do |track|
       # for each track, check if it's already in the DB, if not create the song
-      song = Song.find_by(spotify_id: track.id)
-
-      if song.nil?
-        features = RSpotify::AudioFeatures.find(track.id)
-        
-        song = Song.create do |song|
-          song.spotify_id = track.id
-          song.acousticness = features.acousticness
-          song.danceability = features.danceability
-          song.energy = features.energy
-          song.tempo = features.tempo
-          song.valence = features.valence
-          song.name = track.name
-          song.uri = track.uri
-          song.artist = track.artists.first.name
-        end
-      end
+      song = Song.find_by(spotify_id: track.id) 
+      song = create_song(track) if song.nil? 
 
       # check if song-user combo exists, if not insert that into the join table
       SongsUser.find_or_create_by(song: song, user: current_user)
+    end
+  end
+
+  def create_song(track)
+    features = RSpotify::AudioFeatures.find(track.id)
+
+    Song.create do |song|
+      song.acousticness = features.acousticness
+      song.danceability = features.danceability
+      song.energy = features.energy
+      song.tempo = features.tempo
+      song.valence = features.valence
+      song.spotify_id = track.id
+      song.name = track.name
+      song.uri = track.uri
+      song.artist = track.artists.first.name
     end
   end
 end
