be84d2e99f62b3677738975096fe2f145da4a3ee
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-22 01:12:19 +0800

    Refactor self.call to create new instance and use the instance call method

--- app/services/make_spotify_playlist_service.rb
+++ app/services/make_spotify_playlist_service.rb
@@ -1,15 +1,26 @@
 class MakeSpotifyPlaylistService
   def self.call(song_uris:, current_user:, playlist_params:, image_url:)
-    playlist_image = URI.open(image_url)
+    new(song_uris:, current_user:, playlist_params:, image_url:).call
+  end
+  
+  def initialize(song_uris:, current_user:, playlist_params:, image_url:)
+    @song_uris = song_uris
+    @current_user = current_user 
+    @playlist_params = playlist_params
+    @image_url = image_url
+  end
+
+  def call
+    playlist_image = URI.open(@image_url)
     # song_uris, current_user, @ss_playlist
     # make empty playlist in spotify playlist
-    spotify_user = current_user.spotify_user
-    spotify_playlist = spotify_user.create_playlist!(playlist_params[:title])
+    spotify_user = @current_user.spotify_user
+    spotify_playlist = spotify_user.create_playlist!(@playlist_params[:title])
     # add tracks to spotify
-    spotify_playlist.add_tracks!(song_uris)
+    spotify_playlist.add_tracks!(@song_uris)
 
     # add playlist to our DB
-    ss_playlist = Playlist.new(title: playlist_params[:title], user: current_user, spotify_id: spotify_playlist.id)
+    ss_playlist = Playlist.new(title: @playlist_params[:title], user: @current_user, spotify_id: spotify_playlist.id)
 
     # attaching AI image to our DB
     ss_playlist.photo.attach(io: playlist_image, filename: "#{ss_playlist.title}.png", content_type: "image/png")
