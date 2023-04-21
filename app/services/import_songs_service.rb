dadaa5f519cc9d2da871c99c03b8a4237de82062
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-22 02:41:15 +0800

    Only fetch features and create song if song not found in the table

--- app/services/import_songs_service.rb
+++ app/services/import_songs_service.rb
@@ -20,20 +20,26 @@ class ImportSongsService
   def self.insert_songs(all_tracks, current_user)
     all_tracks.each do |track|
       # for each track, check if it's already in the DB, if not create the song
-      @song = Song.find_or_create_by(spotify_id: track.id) do |song|
+      song = Song.find_by(spotify_id: track.id)
+
+      if song.nil?
         features = RSpotify::AudioFeatures.find(track.id)
-        song.acousticness = features.acousticness
-        song.danceability = features.danceability
-        song.energy = features.energy
-        song.tempo = features.tempo
-        song.valence = features.valence
-        song.name = track.name
-        song.uri = track.uri
-        song.artist = track.artists.first.name
+        
+        song = Song.create do |song|
+          song.spotify_id = track.id
+          song.acousticness = features.acousticness
+          song.danceability = features.danceability
+          song.energy = features.energy
+          song.tempo = features.tempo
+          song.valence = features.valence
+          song.name = track.name
+          song.uri = track.uri
+          song.artist = track.artists.first.name
+        end
       end
 
       # check if song-user combo exists, if not insert that into the join table
-      SongsUser.find_or_create_by(song: @song, user: current_user)
+      SongsUser.find_or_create_by(song: song, user: current_user)
     end
   end
 end
