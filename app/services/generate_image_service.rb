e388a04ad09d916bb4df6c48805d468374041ba0
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-21 18:00:50 +0800

    refactors mood_descriptors to extract boolean conditions to boolean methods

--- app/services/generate_image_service.rb
+++ app/services/generate_image_service.rb
@@ -25,7 +25,7 @@ class GenerateImageService
     prompt = generate_prompt(@song)
 
     # Generate image and returns image url.
-    image_response = client.images.generate(parameters: { prompt: "#{prompt}, #{mood_descriptors(@event).sample(2).join(', ')}, in the art style of #{ART_STYLES.sample}. use a colour palette inspired by #{TIME_COLOURS[@event.time.to_sym].sample}", size: "256x256" })
+    image_response = client.images.generate(parameters: { prompt: "#{prompt}, #{get_mood_descriptors.sample(2).join(', ')}, in the art style of #{ART_STYLES.sample}. use a colour palette inspired by #{TIME_COLOURS[@event.time.to_sym].sample}", size: "256x256" })
 
     # Return the url of the image generated.
     img_res = image_response.dig('data', 0, 'url')
@@ -33,34 +33,16 @@ class GenerateImageService
 
   private
 
+  attr_reader :song, :event
+
   def client
     @client ||= OpenAI::Client.new
   end
-
-  # Helper method to get descriptions to help enhance the image generated.
-  def mood_descriptors(event)
-    # very sad songs
-    if event.max_valence < 0.4
-      %w[winter somber melancholic sad gothic post-apocalyptic poignant]
-    # happy energetic, less acoustic
-    elsif event.max_valence > 0.6 && (event.max_energy > 0.6 || event.max_acousticness < 0.5)
-      %w[bright vibrant dynamic spirited vivid lively energetic colorful joyful romantic expressive rich]
-    # happy chill, more acoustic
-    elsif event.max_valence > 0.6 && (event.max_energy < 0.6 || event.max_acousticness > 0.5)
-      %w[light peaceful calm serene soothing relaxed cosy tranquil pastel ethereal tender soft]
-    # neutral
-    elsif event.max_valence < 0.6
-      %w[harmonious natural diffuse whimsical]
-    # discordant moods
-    else
-      %w[sublime symmetrical cool nostalgic]
-    end
-  end
-
+  
   # Private method to generate prompt from the Song instance object.
   def generate_prompt(song)
     query = "Return the mood of the song #{song.name} by #{song.artist} in three words."
-
+    
     response = client.chat(
       parameters: {
         model: 'gpt-3.5-turbo', # Required.
@@ -71,4 +53,34 @@ class GenerateImageService
     response.dig('choices', 0, 'message', 'content')
   end
 
+  # Helper method to get descriptions to help enhance the image generated.
+  def get_mood_descriptors
+    if very_sad?
+      %w[winter somber melancholic sad gothic post-apocalyptic poignant]
+    elsif neutral?
+      %w[harmonious natural diffuse whimsical]
+    elsif happy_energetic_less_acoustic?
+      %w[bright vibrant dynamic spirited vivid lively energetic colorful joyful romantic expressive rich]
+    elsif happy_chill_more_acoustic?
+      %w[light peaceful calm serene soothing relaxed cosy tranquil pastel ethereal tender soft]
+    else
+      %w[sublime symmetrical cool nostalgic]
+    end
+  end
+
+  def very_sad?
+    event.max_valence < 0.4
+  end
+
+  def neutral?
+    event.max_valence < 0.6
+  end
+
+  def happy_energetic_less_acoustic?
+    event.max_valence > 0.6 && (event.max_energy > 0.6 || event.max_acousticness < 0.5)
+  end
+
+  def happy_chill_more_acoustic?
+    event.max_valence > 0.6 && (event.max_energy < 0.6 || event.max_acousticness > 0.5)
+  end
 end
