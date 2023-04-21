0a9841e228635e63457bb1f05695188a01941448
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-22 01:17:50 +0800

    Make private attr_reader for all instance variables and private method to get spotify_user

--- app/services/make_spotify_playlist_service.rb
+++ app/services/make_spotify_playlist_service.rb
@@ -14,7 +14,6 @@ class MakeSpotifyPlaylistService
     playlist_image = URI.open(@image_url)
     # song_uris, current_user, @ss_playlist
     # make empty playlist in spotify playlist
-    spotify_user = @current_user.spotify_user
     spotify_playlist = spotify_user.create_playlist!(@playlist_params[:title])
     # add tracks to spotify
     spotify_playlist.add_tracks!(@song_uris)
@@ -25,5 +24,13 @@ class MakeSpotifyPlaylistService
     # attaching AI image to our DB
     ss_playlist.photo.attach(io: playlist_image, filename: "#{ss_playlist.title}.png", content_type: "image/png")
     return [ss_playlist, spotify_playlist]
+  end
+
+  private
+
+  attr_reader :song_uris, :current_user, :playlist_params, :image_url
+
+  def spotify_user
+    @spotify_user ||= current_user.spotify_user
   end
 end
