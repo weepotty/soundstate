87973c0eeeee38e685d40b897611382e14ace92b
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-04-22 13:18:22 +0800

    Only import the first 400 songs when the current_user is a new user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,14 +12,24 @@ class PagesController < ApplicationController
 
   def load_songs
     # get all the songs from user's library and their properties, and load into songs table
-    ::ImportSongsService.call(
-      spotify_user: current_user.spotify_user,
-      current_user: current_user
-    )
+    if new_user?
+      ::ImportSongsService.call(
+        spotify_user: current_user.spotify_user,
+        current_user: current_user
+      )
+    else
+    end
 
     # Sync user's playlists with Spotify as a background job.
     UpdatePlaylistsJob.perform_later(current_user)
 
     redirect_to root_path
+  end
+
+  private
+
+  def new_user?
+    # User created within the last 60 seconds
+    (Time.now - current_user.created_at) < 60
   end
 end
