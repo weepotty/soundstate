9d96bbd33569396b8fd9c83379eb170d749b9a8c
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-27 18:52:44 +0800

    Update email_regexp in devise.rb to correctly invalidate user with incorrectly formatted email address

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe User, type: :model do
 
     expect(user.errors[:email].empty?).to be(true)
   end
+
+  it "is an invalid user if email is incorrectly formatted" do
+    user1 = User.new(email: 'alice@alice')
+    user2 = User.new(email: 'alice.com')
+
+    user1.valid?
+    user2.valid?
+
+    expect(user1.errors[:email].empty?).to be(false)
+    expect(user2.errors[:email].empty?).to be(false)
+  end
 end
