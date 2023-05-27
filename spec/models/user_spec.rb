bced23910c877fca60c39875e7a189c64822fbc2
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-27 19:09:02 +0800

    Refactor the tests for emails to be in a nested describe block for readability

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,39 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "must have a correctly formatted email address" do
-    user = User.new(email: 'alice@alice.com')
-    
-    user.valid?
 
-    expect(user.errors[:email].empty?).to be(true)
-  end
-
-  it "is invalid if email is incorrectly formatted" do
-    user1 = User.new(email: 'alice@alice')
-    user2 = User.new(email: 'alice.com')
-
-    user1.valid?
-    user2.valid?
-
-    expect(user1.errors[:email][0]).to eq("is invalid")
-    expect(user2.errors[:email][0]).to eq("is invalid")
-  end
-
-  it "is invalid if email is empty" do
-    user = User.new
-
-    user.valid?
-
-    expect(user.errors[:email][0]).to eq("can't be blank")
-  end
-
-  it "should have unique email" do
-    FactoryBot.create(:user)
-    user = FactoryBot.build(:user)
-
-    user.valid?
-
-    expect(user.errors[:email][0]).to eq("has already been taken")
+  describe 'email' do
+    it "must be correctly formatted" do
+      user = User.new(email: 'alice@alice.com')
+      
+      user.valid?
+  
+      expect(user.errors[:email].empty?).to be(true)
+    end
+  
+    it "is invalid if incorrectly formatted" do
+      user1 = User.new(email: 'alice@alice')
+      user2 = User.new(email: 'alice.com')
+  
+      user1.valid?
+      user2.valid?
+  
+      expect(user1.errors[:email][0]).to eq("is invalid")
+      expect(user2.errors[:email][0]).to eq("is invalid")
+    end
+  
+    it "can't be blank" do
+      user = User.new
+  
+      user.valid?
+  
+      expect(user.errors[:email][0]).to eq("can't be blank")
+    end
+  
+    it "should be unique" do
+      FactoryBot.create(:user)
+      user = FactoryBot.build(:user)
+  
+      user.valid?
+  
+      expect(user.errors[:email][0]).to eq("has already been taken")
+    end
   end
 end
